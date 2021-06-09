bd5758f73a95f2e7bdefb5c56eeb369887e45d39
40oleg <60510512+40oleg@users.noreply.github.com>
2021-06-09 21:58:12 +0300

    Add files via upload

--- js/select.js
+++ js/select.js
@@ -2,6 +2,11 @@ $('.select').on('click', function(e) {
     $(this).toggleClass('select_active');
 });
 
+$('.select__div-input').on('click', function() {
+    $('.select__div-input').focus()
+})
+
 $('.select__option').on('click', function(e) {
-    $(this).parent('.select__options').parent('.select').children('.select__value').text($(this).text());
+    $(this).parent('.select__options').parent('.select').children('.select__value').html($(this).html());
+    $(this).parent('.select__options').parent('.select').children('.select__value').children('.select__div-input').focus()
 })
